889cea5a1efbfc193df53a03df5fddd51fbedf39
Ryan Malloy <ryan@supported.systems>
2024-11-13 20:36:26 -0700

    add huey tasks

--- channels_presence/tasks.py
+++ channels_presence/tasks.py
@@ -19,3 +19,23 @@ except ImportError:
         You tried to use Celery tasks but it is not installed.
         """
     )
+
+
+try:
+    from huey import crontab
+    from huey.contrib.djhuey import db_periodic_task, db_task
+
+    @db_periodic_task(crontab(minute='*/1'))
+    def prune_presence():
+        Room.objects.prune_presences()
+
+    @db_periodic_task(crontab(minute='*/10'))
+    def prune_rooms():
+        Room.objects.prune_rooms()
+
+except ImportError:
+    logging.info(
+        """Django Channels Presence 4.0\n
+        You tried to use huey tasks but it is not installed.
+        """
+    )
